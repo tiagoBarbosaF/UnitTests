9d649d59a5ab1c28880113cf8dd8439aef6a521a
Tiago Barbosa <tiago@farias.cc>
2021-09-28 06:26:16 -0300

    Firsts unit tests

---
+++ HelloWorld/HelloWorld/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace HelloWorld
+{
+  public class Tests
+  {
+    private const string Expected = "Hello World!";
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+      // Assert.Pass();
+      using (var sw = new StringWriter())
+      {
+        Console.SetOut(sw);
+        HelloWorldCore.Program.Main();
+
+        var result = sw.ToString().Trim();
+        Assert.AreEqual(Expected, result);
+      }
+    }
+  }
+}
---
+++ HelloWorldCoreTest/HelloWorldTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Net.Http.Headers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorldTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string Expected = "Hello World!";
+
+        [TestMethod]
+        public void TestMehod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                HelloWorldCoreTest.Program.Main();
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(Expected, result);
+            }
+        }
+    }
+
+}
+
+
